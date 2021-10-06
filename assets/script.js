abc2f7e47a4df9e0c6ea42ff967a5c498d71fe00
talysonxx <talysonoficial12@gmail.com>
2021-10-06 16:32:38 -0300

    embaralhando as cartas

--- assets/script.js
+++ assets/script.js
@@ -15,8 +15,25 @@ let techs = [
   'vscode'
 ]
 
-createCardsFromTechs(techs)
+let cards = null
+startGame()
 
+function startGame() {
+    cards = createCardsFromTechs(techs)
+    shuffleCards(cards)
+    console.log(cards)
+}
+function shuffleCards(cards) {
+  let currentIndex = cards.length
+  let randomIndex = 0
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+  }
+}
 function createCardsFromTechs(techs) {
   let cards = []
 
